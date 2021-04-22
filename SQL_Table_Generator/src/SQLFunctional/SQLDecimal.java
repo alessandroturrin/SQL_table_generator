e0b5c042a3f4d37c3cf0fc00716be4b34acb3581
alessandroturrin <alexturrin@gmail.com>
2021-04-22 10:37:42 +0200

    Add files via upload

---
+++ SQL_Table_Generator/src/SQLFunctional/SQLDecimal.java
@@ -0,0 +1,47 @@
+package SQLFunctional;
+
+import java.util.Random;
+
+class SQLDecimal extends Attribute
+{
+	private int precision, scale;
+	
+	public SQLDecimal(String attributeName, String attributeType, int precision, int scale, boolean isReferenceKey)
+	{
+		super(attributeName, attributeType, isReferenceKey);
+		this.precision = precision;
+		this.scale = scale;
+	}
+	
+	@Override
+	public String createAttribute() 
+	{
+		String str = attributeName;
+		str = str + " DECIMAL(" + precision + ", " + scale + ")";
+		return str;
+	}
+
+	@Override
+	public String generateAttribute() 
+	{
+		Random rand = new Random();
+		Double value = rand.nextDouble();
+		listOfAttributes.add(value.toString());
+		return value.toString();
+	}
+	
+	@Override
+	public String generateAttribute(String[] values) 
+	{
+		Random rand = new Random();
+		String value = values[rand.nextInt(values.length-1)];
+		listOfAttributes.add(value);
+		return value;
+	}
+	
+	@Override
+	public boolean isPrimaryKey()
+	{
+		return false;
+	}
+}
